884334efe7578ae5bc997c0e21480ea79061af57
Imukata-aH <leahyleahyleahy@gmail.com>
2017-02-15 00:31:21 +0900

    Implement framework.
    - Implemented WinMain().
    - Implemented SystemClass.
    - Added necessary class definitions; GraphicsClass and InputClass.

---
+++ DirectX11_Test/src/main.cpp
@@ -0,0 +1,24 @@
+﻿#include "systemclass.h"
+
+int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pScmdline, int iCmdshow )
+{
+	SystemClass* system { new SystemClass };
+	bool result;
+
+	if ( !system )
+	{
+		return 0;
+	}
+
+	result = system->Initialize();
+	if ( result )
+	{
+		system->Run();
+	}
+
+	system->Shutdown();
+	delete( system );
+	system = 0;
+
+	return 0;
+}
