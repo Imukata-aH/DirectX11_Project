ca1ea2b25a850ca55280565e72c463b9ea2087c2
hayakawa <Takuya.Hayakawa@sony.com>
2017-02-15 17:53:46 +0900

    Implemented framework skeleton.

--- DirectX11_Test/include/graphicsclass.h
+++ DirectX11_Test/include/graphicsclass.h
@@ -1,6 +1,23 @@
 ﻿#pragma once
 
+#include <windows.h>
+
+const bool FULL_SCREEN = false;
+const bool VSYNC_ENABLED = true;
+const float SCREEN_DEPTH = 1000.0f;
+const float SCREEN_NEAR = 0.1f;
+
 class GraphicsClass
 {
+public:
+    GraphicsClass();
+    GraphicsClass(const GraphicsClass&);
+    ~GraphicsClass();
 
+    bool Initialize(int screenWidth, int screenHeight, HWND hwnd);
+    void Shutdown();
+    bool Frame();
+
+private:
+    bool Render();
 };
---
+++ DirectX11_Test/src/inputclass.cpp
@@ -0,0 +1,38 @@
+﻿#include "inputclass.h"
+
+InputClass::InputClass()
+{
+}
+
+InputClass::InputClass(const InputClass& other)
+{
+}
+
+InputClass::~InputClass()
+{
+}
+
+void InputClass::Initialize()
+{
+    int i;
+
+    for( i=0; i < 256; i++ )
+    {
+        m_keys[i] = false;
+    }
+}
+
+void InputClass::KeyUp(unsigned int input)
+{
+    m_keys[input] = true;
+}
+
+void InputClass::KeyDown(unsigned int input)
+{
+    m_keys[input] = false;
+}
+
+bool InputClass::IsKeyDown(unsigned int key)
+{
+    return m_keys[key];
+}
